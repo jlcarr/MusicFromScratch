78a77b872fb0b2776f0ce07d6e8398e4ab2c78c4
Joseph Carr <joseph.carr@mail.mcgill.ca>
2021-05-31 17:21:37 -0400

    Moved the envelopes to own class, with variable shapes. Added notes. Got a nice pluck.

--- README.md
+++ README.md
@@ -8,21 +8,54 @@ A simple music synthesizer created from scratch using SciPy and Numpy.
 - There are 12 notes per octave (including semitones)
 - Therefore any other note's frequency in Hz can be calculated: `440*2**((X-A)/12)`
 
-### Oscillators
+### Synth Building Blocks
+#### Oscillators
 Synthesizers combine fundamental waveforms with filters and other effects to construct their instruments.  
 The fundamental waveforms are:
 - Sine waves
+	- Composed of one harmonic (itself).
+	- This is the "purest" and more simple wave.
 - Square waves
+	- Composed of only odd harmonics, falling off linearly.
+	- Often described as "wooden". Reminiscent of "Chiptunes"
 - Sawtooth waves
+	- Contains all harmonics, falling off linearly.
+	- Often described as "buzzy". Useful for strings.
 - Triangle waves
+	- Composed of only odd harmonics, falling off quadratically.
+	- Not as commonly used.
+- White noise
+	- Atonal: An even distribution over all frequencies.
+	- Generated from a Gaussian distribution.
+	- Sounds like TV static.
 
-### Filters
+#### Envelopes
 
+#### Filters
+
+#### Reverb
+Related concepts are echo and delay.
 
 ## Resources
 ### Python
 - https://python.plainenglish.io/making-a-synth-with-python-oscillators-2cb8e68e9c3b
 - https://docs.scipy.org/doc/scipy/reference/signal.html
 
+### Similar Projects
+- https://www.youtube.com/watch?v=ogFAHvYatWs (Ignio Quilez makes synths with basic maths)
+- https://www.youtube.com/watch?v=vY5iyMa2_VI (song made with Audacity only)
+- https://www.youtube.com/watch?v=iW0lvWs2aFo (song made with Audacity only)
+
 ### Sound Design / Synth
 - https://en.wikipedia.org/wiki/A440_(pitch_standard)
+#### Oscillators
+- https://en.wikipedia.org/wiki/Sawtooth_wave
+#### Envelopes
+- https://en.wikipedia.org/wiki/Envelope_(waves)
+#### Reverb
+- https://en.wikipedia.org/wiki/Reverberation
+- https://en.wikipedia.org/wiki/Convolution_reverb
+#### Filters
+- https://en.wikipedia.org/wiki/Filter_(signal_processing)
+- https://en.wikipedia.org/wiki/Low-pass_filter
+- https://en.wikipedia.org/wiki/Butterworth_filter
--- synth.py
+++ synth.py
@@ -8,15 +8,51 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 
-class oscillator:
-	def __init__(self, amp=1.0, shape='sine', attack=0.5, decay=0.25, sustain=0.5, release=1., voices=1, detune=0.):
-		self.amp = amp
-		self.shape = shape
-
+class envelope:
+	def __init__(self, attack=0.5, decay=0.25, sustain=0.5, release=1., 
+		attack_func=lambda x: x, decay_func=lambda x: 1.-x, release_func=lambda x: 1.-x):
 		self.attack = attack
 		self.decay = decay
 		self.sustain = sustain
 		self.release = release
+
+		self.attack_func = attack_func
+		self.decay_func = decay_func
+		self.release_func = release_func
+
+	def gen_envelope(self, hold, samplerate=44100):
+		t_tot = hold + self.release
+		size = int(t_tot*samplerate)
+		envelope = np.zeros(size)
+
+		t_start, t_end = 0, min(self.attack, hold)
+		start, end = int(t_start*samplerate), int(t_end*samplerate)
+		envelope[start:end] = self.attack_func(np.linspace(0., 1., end-start))
+
+		if hold > self.attack:
+			t_start, t_end = t_end, min(self.attack+self.decay,hold)
+			start, end = int(t_start*samplerate), int(t_end*samplerate)
+			envelope[start:end] = self.sustain + (1-self.sustain)*self.decay_func(np.linspace(0., 1., end-start))
+
+		if hold > self.attack+self.decay:
+			t_start, t_end = t_end, hold
+			start, end = int(t_start*samplerate), int(t_end*samplerate)
+			envelope[start:end] = self.sustain*np.ones(end-start)
+
+		t_start, t_end = t_end, t_end+self.release
+		start, end = int(t_start*samplerate), int(t_end*samplerate)
+		envelope[start:end] = self.sustain*self.release_func(np.linspace(0., 1., end-start))
+
+		return envelope
+		
+		
+
+class oscillator:
+	def __init__(self, amp=1.0, shape='sine', note_envelope=envelope(), voices=1, detune=0.):
+		self.amp = amp
+		self.shape = shape
+
+		self.note_envelope = note_envelope
 
 		self.voices = voices
 		self.detune = detune
@@ -31,30 +67,10 @@ class oscillator:
 		self.reverb.append(impulse_response)
 
 	def play_note(self, fs, hold, samplerate=44100):
-		t_tot = hold + self.release
+		t_tot = hold + self.note_envelope.release
 		size = int(t_tot*samplerate)
 		t = np.linspace(0., t_tot, size)
 		data = np.zeros(size)
-		envelope = np.zeros(size)
-
-		t_start, t_end = 0, min(self.attack,hold)
-		start, end = int(t_start*samplerate), int(t_end*samplerate)
-		envelope[start:end] = np.linspace(0., 1., end-start)
-
-		if hold > self.attack:
-			t_start, t_end = t_end, min(self.attack+self.decay,hold)
-			start, end = int(t_start*samplerate), int(t_end*samplerate)
-			envelope[start:end] = np.linspace(1., self.sustain, end-start)
-
-		if hold > self.attack+self.decay:
-			t_start, t_end = t_end, hold
-			start, end = int(t_start*samplerate), int(t_end*samplerate)
-			envelope[start:end] = self.sustain*np.ones(end-start)
-
-		t_start, t_end = t_end, t_end+self.release
-		start, end = int(t_start*samplerate), int(t_end*samplerate)
-		envelope[start:end] = self.sustain*np.linspace(1., 0., end-start)
-		
 
 		wave_func = np.sin
 		if self.shape == 'sawtooth':
@@ -71,6 +87,7 @@ class oscillator:
 			data += wave_func(2. * np.pi * fs * 2**(voice*self.detune) * t)
 			data += wave_func(2. * np.pi * fs * 2**(-voice*self.detune) * t)
 		
+		envelope = self.note_envelope.gen_envelope(hold, samplerate=samplerate)
 		data *= envelope
 
 		for sos in self.filters:
@@ -93,7 +110,7 @@ class synth:
 
 
 	def play_notes(self, fs, hold = 1.):
-		t_tot = hold + max([osc.release for osc in self.oscillators])
+		t_tot = hold + max([osc.note_envelope.release for osc in self.oscillators])
 		size = int(t_tot*self.samplerate)
 		t = np.linspace(0.0, t_tot, size)
 		data = np.zeros(size)
@@ -110,7 +127,8 @@ fs = 261.63
 t_tot = 4.
 s = synth()
 
-osc = oscillator(shape='sawtooth', attack=1.5, decay=0.0, sustain=1.0, release=3., voices=5, detune = 0.01)
+note_envelope = envelope(attack=1.5, decay=0.0, sustain=1.0, release=3.)
+osc = oscillator(shape='sawtooth', note_envelope=note_envelope, voices=5, detune = 0.01)
 sos = signal.butter(2, fs, 'lowpass', fs=samplerate, output='sos')
 osc.add_filter(sos)
 #impulse_response = np.exp(-np.linspace(0., 20., 1*samplerate))
@@ -121,7 +139,8 @@ osc.add_filter(sos)
 #osc.add_reverb(impulse_response)
 s.add_oscillator(osc)
 
-osc = oscillator(shape='whitenoise', amp=0.03, attack=1.5, decay=0.0, sustain=1.0, release=3)
+note_envelope = envelope(attack=1.5, decay=0.0, sustain=1.0, release=3)
+osc = oscillator(shape='whitenoise', amp=0.03, note_envelope=note_envelope)
 sos = signal.butter(2, 2*2*fs, 'lowpass', fs=samplerate, output='sos')
 osc.add_filter(sos)
 s.add_oscillator(osc)
@@ -144,7 +163,8 @@ fs = 261.63
 t_tot = 2.0
 s = synth()
 
-osc = oscillator(shape='swtooth', attack=0.05, decay=0.3, sustain=0.0, voices=3, detune=0.01)
+note_envelope = envelope(attack=0.05, decay=0.1, sustain=0.0)
+osc = oscillator(shape='sawtooth', note_envelope=note_envelope, voices=3, detune=0.01)
 sos = signal.butter(2, fs, 'lowpass', fs=samplerate, output='sos')
 osc.add_filter(sos)
 impulse_response = np.zeros(samplerate)
@@ -156,5 +176,23 @@ s.add_oscillator(osc)
 data = s.play_notes(fs, t_tot)
 data *= np.iinfo(np.int16).max / np.max(np.abs(data))
 wavfile.write("example.wav", samplerate, data.astype(np.int16))
-playsound('example.wav')
+#playsound('example.wav')
 
+#sys.exit()
+
+
+samplerate=44100
+fs = 261.63
+t_tot = 2.0
+s = synth()
+
+note_envelope = envelope(attack=0.0, decay=1.5, sustain=0.0, decay_func=lambda x: np.exp(-8*x))
+#note_envelope = envelope(attack=0, decay=0, sustain=1.0, release=0)
+osc = oscillator(shape='sine', note_envelope=note_envelope)
+s.add_oscillator(osc)
+
+data = s.play_notes(fs, t_tot)
+
+data *= np.iinfo(np.int16).max / np.max(np.abs(data))
+wavfile.write("example.wav", samplerate, data.astype(np.int16))
+playsound('example.wav')
