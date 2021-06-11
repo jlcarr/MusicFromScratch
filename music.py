87ac1602fa107a3acf3f77df14d094c0619914f1
Joseph Carr <joseph.carr@mail.mcgill.ca>
2021-06-10 22:13:57 -0400

    Added treatment of sharps in

--- music.py
+++ music.py
@@ -14,7 +14,9 @@ degree_types = {'I':0, 'ii':1, 'iii': 2, 'IV': 3, 'V': 4, 'vi':5, 'vii':6}
 def gen_chord(name, octave=4, inversion=0, hold=1, time=0, arpeggio=0):
 	root = name[0]
 	root = scale_types['major'][ord(root) - ord('C')] 
-	mod = name[1:]
+	root += 1 if '#' in name else 0
+	mod = name[2:] if '#' in name else name[1:]
+	print(root, mod)
 
 	root = 12*(octave+1) + root
 
@@ -23,6 +25,10 @@ def gen_chord(name, octave=4, inversion=0, hold=1, time=0, arpeggio=0):
 		chord = [0,4,7]
 	elif mod == 'm':
 		chord = [0,3,7]
+	elif mod == 'dim':
+		chord = [0,3,6]
+	elif mod == 'aug':
+		chord = [0,4,8]
 
 	if inversion == 1:
 		chord[1],chord[2] = chord[1]-12,chord[2]-12
@@ -47,9 +53,11 @@ def gen_progression(key='C', degree='I', octave=4, inversion=0, time=0, hold=1, 
 		name += '#'
 	if minor:
 		name += 'm'
+	print(name)
 	return gen_chord(name, octave=octave, inversion=inversion, time=time, hold=hold, arpeggio=arpeggio)
 
 
+key = 'C'
 samplerate=44100
 fs = 261.63
 s = synth(bpm=100)
@@ -73,10 +81,10 @@ osc.add_filter(sos)
 s.add_oscillator(osc)
 
 music = []
-music += gen_progression(key='C', degree='vi', inversion=2, time=0, hold=4)
-music += gen_progression(key='C', degree='ii', time=4, hold=4)
-music += gen_progression(key='C', degree='V', inversion=1, time=8, hold=4)
-music += gen_progression(key='C', degree='I', time=12, hold=4)
+music += gen_progression(key=key, degree='vi', inversion=2, time=0, hold=4)
+music += gen_progression(key=key, degree='ii', time=4, hold=4)
+music += gen_progression(key=key, degree='V', inversion=1, time=8, hold=4)
+music += gen_progression(key=key, degree='I', time=12, hold=4)
 
 music += [{**note, 'time':note['time']+16} for i,note in enumerate(music)]
 print(music)
@@ -100,21 +108,18 @@ osc.add_filter(sos)
 s.add_oscillator(osc)
 
 music = []
-music += gen_progression(key='C', degree='vi', octave=5, time=0, hold=1)
-music += gen_progression(key='C', degree='ii', octave=5, time=4, hold=1)
-music += gen_progression(key='C', degree='V', octave=5, time=8, hold=1)
-music += gen_progression(key='C', degree='I', octave=5, time=12, hold=1)
-#print(music)
+music += gen_progression(key=key, degree='vi', octave=5, time=0, hold=1, arpeggio=0.25)
+music += gen_progression(key=key, degree='ii', octave=5, time=4, hold=1, arpeggio=0.25)
+music += gen_progression(key=key, degree='V', octave=5, time=8, hold=1, arpeggio=0.25)
+music += gen_progression(key=key, degree='I', octave=5, time=12, hold=1, arpeggio=0.25)
 
-
-music = [{**note, 'time':note['time']+2*(i%3)/4}  for i,note in enumerate(music)]
 	#music += [{**note, 'time':note['time']+1} for i,note in enumerate(music)]
 music += [{**note, 'time':note['time']+2} for i,note in enumerate(music)]
 music += [{**note, 'time':note['time']+16} for i,note in enumerate(music)]
 lead_data = s.play_song(music)
-data[:lead_data.size] += lead_data
+data[:lead_data.size] += 0.8*lead_data
 
 
-data *= np.iinfo(np.int16).max / np.max(np.abs(data))
+data *= 0.8*np.iinfo(np.int16).max / np.max(np.abs(data))
 wavfile.write("example.wav", samplerate, data.astype(np.int16))
 playsound('example.wav')
